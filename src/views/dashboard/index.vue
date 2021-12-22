f3d1062f212a0c6f6d8a347e9459ea9a634eddf2
李惠 <lihuiyf@seeyon.com>
2021-12-22 11:03:46 +0800

    xiuga首页

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="dashboard-container flex-start-start-column">
-    <a href="http://101.132.162.18/villageStandard/#/login" target="_blank" rel="noopener">智慧社区综合管理平台（账号：手机号18800204167，密码：123456）</a>
-    <a href="http://101.132.162.18/property/#/login" target="_blank" rel="noopener">红色物业智慧房管平台（账号：admin，密码：123456）</a>
-    <a href="http://139.196.240.47/refuse/#/login" target="_blank" rel="noopener">智能垃圾分类监管服务系统（账号：admin，密码：123456）</a>
-    <a href="http://192.168.0.81/throw/?#/login" target="_blank" rel="noopener">社区高空抛物监管服务平台（账号：15021245602，密码：123456）</a>
-    <a href="http://101.132.162.18/carAdmin/#/login" target="_blank" rel="noopener">智慧车管综合管理平台（账号：admin/手机号，密码：123456）</a>
+    暂无
   </div>
 </template>
 
